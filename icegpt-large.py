fdbd8f8617132caddb96e5cc4ad6f96c6dd4c3b2
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2023-08-19 20:19:53 +0000

    the script works and has been refactored

--- icegpt-large.py
+++ icegpt-large.py
@@ -1,95 +1,54 @@
-from pynvml import *
+from transformers import (
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
+    GPT2Config,
+    GPT2LMHeadModel,
+    Trainer,
+    TrainingArguments,
+    AdamW,
+)
+from transformers.models.gpt2.tokenization_gpt2 import bytes_to_unicode
+from torch.utils.data.dataloader import DataLoader
+from datasets import load_dataset
+from utils import print_gpu_utilization, print_summary, get_all_paths_in_dir
+from tokenizer import train_new_tokenizer
 
+# Suppress warnings
+print("\n" * 3)
 
-def print_gpu_utilization():
-    nvmlInit()
-    handle = nvmlDeviceGetHandleByIndex(0)
-    info = nvmlDeviceGetMemoryInfo(handle)
-    print(f"GPU memory occupied: {info.used//1024**2} MB.")
-
-
-def print_summary(result):
-    print(f"Time: {result.metrics['train_runtime']:.2f}")
-    print(f"Samples/second{result.metrics['train_samples_per_second']:.2f}")
-    print_gpu_utilization()
-
-
+# Print GPU utilization
 print_gpu_utilization()
 
-
-from transformers import AutoTokenizer
-from transformers.models.gpt2.tokenization_gpt2 import bytes_to_unicode
-
-# Base tokenizer
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-base_vocab = list(bytes_to_unicode().values())
-
-
-# Training and saving
-new_tokenizer = tokenizer.train_new_from_iterator(
-    all_paths, vocab_size=30000, initial_alphabet=base_vocab, show_progress=True
-)
-
-new_tokenizer.add_special_tokens(
-    {
-        "eos_token": "<|endoftext|>",
-        "pad_token": "<pad>",
-    }
-)
-
-new_tokenizer.save_pretrained("ice-tokenizer-large")
-
-
-# load tokenizer
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained("./ice-tokenizer-large")
-
-# tokenizer.pad_token = "<pad>"
-
-
-# tokenizer('Hvað er að frétta 😁 ?')
-
-# get special tokens
-tokenizer.cls_token, tokenizer.sep_token, tokenizer.unk_token, tokenizer.pad_token, tokenizer.mask_token
-
-
-import os
-
-all_paths = []
-
+# Collect paths
 print("Collecting all paths...")
-for root, dirs, files in os.walk("./processed_data"):
-    for file in files:
-        if file.endswith(".txt"):
-            all_paths.append(os.path.join(root, file))
+all_paths = get_all_paths_in_dir("./processed_data", max_paths=1000)
 
+# Load or train tokenizer
+# tokenizer = AutoTokenizer.from_pretrained("gpt2")
+# base_vocab = list(bytes_to_unicode().values())
+# new_tokenizer = train_new_tokenizer(tokenizer, all_paths, base_vocab, path="./ice-tokenizer-large")
 
-from datasets import load_dataset
+# Load tokenizer
+print("Loading tokenizer...")
+tokenizer = AutoTokenizer.from_pretrained("./ice-tokenizer-large")
+print("Done.", tokenizer)
 
-portion = 1
-all_paths = all_paths[: int(len(all_paths) * portion)]
-
+# Split train and test paths
 train_paths = all_paths[: int(len(all_paths) * 0.8)]
 test_paths = all_paths[int(len(all_paths) * 0.8) :]
 
-# train and test split
+# Load dataset
 dataset = load_dataset("text", data_files={"train": train_paths, "test": test_paths})
-dataset
 
-
+# GPU utilization
 print_gpu_utilization()
 
-
-tokenizer(
-    "I", truncation=False, padding="max_length", max_length=5, return_tensors="pt"
-)
-
-
-import torch
-from transformers import DataCollatorForLanguageModeling
-
 context_length = 512
+
+# Data collator
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=False, pad_to_multiple_of=None
+)
 
 
 def tokenize_function(examples):
@@ -97,19 +56,13 @@ def tokenize_function(examples):
         examples["text"],
         truncation=True,
         max_length=context_length,
-        padding="max_length",  # padds to the right by default
-        # return_overflowing_tokens=True,
-        # return_length=True,
+        padding="max_length",
     )
     return outputs
 
 
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer, mlm=False, pad_to_multiple_of=None
-)
-
-
-tokanized_dataset = dataset.map(
+# Tokenize dataset
+tokenized_dataset = dataset.map(
     tokenize_function,
     batched=True,
     batch_size=1000,
@@ -117,18 +70,9 @@ tokanized_dataset = dataset.map(
     remove_columns=["text"],
 )
 
-
-tokanized_dataset
-
-
-tokanized_dataset["train"][2]["input_ids"]  # 1024
-
-
-from transformers import GPT2Config, GPT2LMHeadModel
-
 scale = 1
 
-# Initializing a GPT2 configuration
+# Model configuration
 configuration = GPT2Config(
     vocab_size=len(tokenizer),
     n_ctx=context_length,
@@ -139,29 +83,14 @@ configuration = GPT2Config(
     n_head=int(12 * scale),
 )
 
-# Initializing a model from the configuration
+# Initialize model
 model = GPT2LMHeadModel(configuration)
 
-# Accessing the model configuration
-configuration = model.config
-
-
-model.config
-
-
+# Model size
 model_size = sum(t.numel() for t in model.parameters())
 print(f"ICE GPT-2 size: {model_size/1000**2:.1f}M parameters")
 
-
-model.to("cuda")
-
-print_gpu_utilization()
-
-
-from transformers import Trainer, TrainingArguments
-from transformers import AdamW
-
-
+# Training arguments
 training_args = TrainingArguments(
     output_dir="icegpt-large",
     per_device_train_batch_size=1,
@@ -179,6 +108,7 @@ training_args = TrainingArguments(
     fp16=True,
 )
 
+# AdamW optimizer
 adam_bnb_optim = AdamW(
     model.parameters(),
     lr=training_args.learning_rate,
@@ -187,101 +117,27 @@ adam_bnb_optim = AdamW(
     weight_decay=training_args.weight_decay,
 )
 
-
-from accelerate import Accelerator
-from torch.utils.data.dataloader import DataLoader
-import torch
-
+# DataLoader
 dataloader = DataLoader(
-    tokanized_dataset["train"], batch_size=training_args.per_device_train_batch_size
+    tokenized_dataset["train"], batch_size=training_args.per_device_train_batch_size
 )
 
+# Optional gradient checkpointing
 if training_args.gradient_checkpointing:
     model.gradient_checkpointing_enable()
 
-
-accelerator = Accelerator()
-
-
+# Trainer
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokanized_dataset["train"],
-    eval_dataset=tokanized_dataset["test"],
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["test"],
     tokenizer=tokenizer,
     data_collator=data_collator,
 )
 
+# Start training
 trainer.train()
 
-
+# Save trained model
 model.save_pretrained("ice-gpt2")
-
-
-from transformers import TextGenerationPipeline
-
-# Load the trained model
-model_path = "ice-gpt2"
-model = GPT2LMHeadModel.from_pretrained(model_path)
-model.eval()
-
-# Create a text generation pipeline
-pipeline = TextGenerationPipeline(model=model, tokenizer=tokenizer)
-
-# Generate text
-generated_text = pipeline(
-    "hæ, hæ",
-    max_length=1000,
-    num_return_sequences=1,
-    do_sample=True,
-    top_k=50,
-    top_p=0.95,
-    temperature=0.9,
-)
-
-# Print the generated text
-print(generated_text[0]["generated_text"])
-
-
-from matplotlib import pyplot as plt
-import pandas as pd
-import numpy as np
-
-df = pd.DataFrame(trainer.state.log_history)
-df.to_csv("./ice-gpt2/log_history.csv")
-
-training_loss_data = []
-eval_loss_data = []
-for i in range(len(df)):
-    row = df.iloc[i]
-    epoch, train_loss, eval_loss = row["epoch"], row["loss"], row["eval_loss"]
-
-    if not np.isnan(eval_loss):
-        eval_loss_data.append((epoch, eval_loss))
-
-    if not np.isnan(train_loss):
-        training_loss_data.append((epoch, train_loss))
-
-# Extract x and y values for training and evaluation losses
-training_epochs, training_losses = zip(*training_loss_data)
-eval_epochs, eval_losses = zip(*eval_loss_data)
-
-# Create a figure and axis
-fig, ax = plt.subplots()
-
-# Plot the training loss
-ax.plot(training_epochs, training_losses, label="Training Loss")
-
-# Plot the evaluation loss
-ax.plot(eval_epochs, eval_losses, label="Evaluation Loss")
-
-# Set labels and title
-ax.set_xlabel("Epochs")
-ax.set_ylabel("Loss")
-ax.set_title("Training and Evaluation Loss")
-
-# Add a legend
-ax.legend()
-
-# Display the plot
-plt.show()
---
+++ utils.py
@@ -0,0 +1,27 @@
+from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo
+import os
+
+
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+
+def print_summary(result):
+    print(f"Time: {result.metrics['train_runtime']:.2f}")
+    print(f"Samples/second{result.metrics['train_samples_per_second']:.2f}")
+    print_gpu_utilization()
+
+
+def get_all_paths_in_dir(path, max_paths=None):
+    all_paths = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".txt"):
+                all_paths.append(os.path.join(root, file))
+            if max_paths is not None and len(all_paths) >= max_paths:
+                break
+
+    return all_paths
