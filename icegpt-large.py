e6f1591185bb9a3a44f2232f45729536e5056f8a
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2023-08-20 00:13:25 +0000

    to be continued... deepspeed works, but Out Of Memory

--- icegpt-large.py
+++ icegpt-large.py
@@ -8,10 +8,25 @@ from transformers import (
     AdamW,
 )
 from transformers.models.gpt2.tokenization_gpt2 import bytes_to_unicode
-from torch.utils.data.dataloader import DataLoader
 from datasets import load_dataset
 from utils import print_gpu_utilization, print_summary, get_all_paths_in_dir
 from tokenizer import train_new_tokenizer
+
+
+"""
+To run this script you need to run the following command:
+
+deepspeed --num_gpus=1 ./icegpt-large.py \
+--deepspeed ./ds_config.json \
+--model_name_or_path icegpt-xl --per_device_train_batch_size 1 \
+--output_dir output_dir --overwrite_output_dir --fp16 \
+--do_train --max_train_samples 500 --num_train_epochs 1 \
+--dataset_name igc
+
+"""
+
+scale = 1
+output_dir = "icegpt-xl"
 
 # Suppress warnings
 print("\n" * 3)
@@ -70,17 +85,27 @@ tokenized_dataset = dataset.map(
     remove_columns=["text"],
 )
 
-scale = 1
 
-# Model configuration
+# Model configuration 140M parameters
+# configuration = GPT2Config(
+#     vocab_size=len(tokenizer),
+#     n_ctx=context_length,
+#     bos_token_id=tokenizer.bos_token_id,
+#     eos_token_id=tokenizer.eos_token_id,
+#     n_embd=int(768),
+#     n_layer=int(12),
+#     n_head=int(12),
+# )
+
+# 1.23B parameters
 configuration = GPT2Config(
     vocab_size=len(tokenizer),
-    n_ctx=context_length,
+    n_positions=context_length,
+    n_embd=1750,
+    n_layer=32,
+    n_head=25,
     bos_token_id=tokenizer.bos_token_id,
     eos_token_id=tokenizer.eos_token_id,
-    n_embd=int(768 * scale),
-    n_layer=int(12 * scale),
-    n_head=int(12 * scale),
 )
 
 # Initialize model
@@ -92,13 +117,13 @@ print(f"ICE GPT-2 size: {model_size/1000**2:.1f}M parameters")
 
 # Training arguments
 training_args = TrainingArguments(
-    output_dir="icegpt-large",
+    output_dir=output_dir,
     per_device_train_batch_size=1,
     per_device_eval_batch_size=1,
     evaluation_strategy="steps",
     eval_steps=5_000,
     logging_steps=5_000,
-    gradient_accumulation_steps=8,
+    gradient_accumulation_steps=4,
     num_train_epochs=1,
     weight_decay=0.1,
     warmup_steps=1_000,
@@ -106,38 +131,24 @@ training_args = TrainingArguments(
     learning_rate=5e-4,
     save_steps=5_000,
     fp16=True,
+    deepspeed="./ds_config.json",
 )
 
-# AdamW optimizer
-adam_bnb_optim = AdamW(
-    model.parameters(),
-    lr=training_args.learning_rate,
-    betas=(0.9, 0.999),
-    eps=1e-8,
-    weight_decay=training_args.weight_decay,
-)
-
-# DataLoader
-dataloader = DataLoader(
-    tokenized_dataset["train"], batch_size=training_args.per_device_train_batch_size
-)
-
-# Optional gradient checkpointing
-if training_args.gradient_checkpointing:
-    model.gradient_checkpointing_enable()
+# Optimizer
+optimizer = AdamW(model.parameters(), lr=5e-4, weight_decay=0.1)
 
 # Trainer
 trainer = Trainer(
     model=model,
     args=training_args,
+    data_collator=data_collator,
     train_dataset=tokenized_dataset["train"],
     eval_dataset=tokenized_dataset["test"],
-    tokenizer=tokenizer,
-    data_collator=data_collator,
+    # optimizer=optimizer,
 )
 
 # Start training
 trainer.train()
 
 # Save trained model
-model.save_pretrained("ice-gpt2")
+model.save_pretrained(output_dir)
